ef7e2b46fdb65a16967e2a59c4bc2c9c69b6972e
Lawando69 <135968948+Lawando69@users.noreply.github.com>
2023-09-20 23:22:39 +0100

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,12 @@
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+day = input("Enter the day of your birth: ")
+month = input("Enter the month of your birth: ")
+year = input("Enter the year of your birth: ")
+
+
+print("Are the details shown bellow correct?")
+print("Name: ", name)
+print("Age: ", age)
+print("DOB: ", day + "/" + month + "/" + year)
