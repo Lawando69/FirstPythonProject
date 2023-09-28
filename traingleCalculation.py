10b9fca06b9b1a6b7647cb7cda8b7147bc4e24da
Lawand Salah <40186936@hruc.ac.uk>
2023-09-28 11:58:16 +0100

    Chaning some properties in the traingle file

--- traingleCalculation.py
+++ traingleCalculation.py
@@ -1,10 +1,6 @@
-triaBase = float(input("Enter the base of the triangle: ")) 
-triaHeight = float(input("Enter the height of the traingle: ")) 
-areaType = input("Is in centimiter or meter? ") 
-
+triaBase = float(input("Enter the base of the triangle in centimiter: ")) 
+triaHeight = float(input("Enter the height of the traingle in centimeter: ")) 
 area = round(triaBase * triaHeight / 2) 
 
-if areaType == "Centimiter": 
-    print(area, "Centimiter squared") 
-elif areaType == "Meter": 
-    print(area, "Meter squared") 
+print("The total area of the traingle: ", area, "Cm squared")
+
