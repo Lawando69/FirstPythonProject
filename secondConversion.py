8dcec5b5aeb4a2eaf89fd1fc18b19b9fb7f1d903
Lawando69 <135968948+Lawando69@users.noreply.github.com>
2023-09-30 18:45:03 +0100

    Delete secondConversion.py

--- secondConversion.py
+++
@@ -1,6 +0,0 @@
-number_sec = int(input("Enter the amount of seconds: "))
-
-calculate_min = number_sec // 60
-calculate_sec = number_sec % 60
-
-print(calculate_min, "minutes", "and", calculate_sec, "seconds")
