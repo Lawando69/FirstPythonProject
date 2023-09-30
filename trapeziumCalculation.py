2a1541e4cf14676fa5dcd7e88cae40e60509f688
Lawando69 <135968948+Lawando69@users.noreply.github.com>
2023-09-30 18:46:06 +0100

    Delete trapeziumCalculation.py

--- trapeziumCalculation.py
+++
@@ -1,7 +0,0 @@
-tra_base = float(input("Enter the length of the base in cm: "))
-tra_top = float(input("Enter the length of the top in cm: "))
-tra_height = float(input("Enter the height in cm: "))
-
-tra_area = (tra_base + tra_top) / 2 * tra_height
-
-print("The area of the trapezium: ", tra_area)
