af758487ca54ac6ace21d0e3cc42a0b0b0f3e818
Lawando69 <135968948+Lawando69@users.noreply.github.com>
2023-09-30 18:44:14 +0100

    Delete compoundInterest.py

--- compoundInterest.py
+++
@@ -1,8 +0,0 @@
-principle_amount = 10000
-compound_per_year = 12
-interest_rate = 0.08
-number_year = int(input("Enter the number of years the money will be compounded for: "))
-
-total = principle_amount * (1 + interest_rate / compound_per_year) ** (compound_per_year * number_year)
-
-print("The total amount: ", total)
